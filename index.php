b4727545bb79f57252593382b2df018c2bbda503
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-30 15:35:11 +0200

    ok

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 2</title>
+  </head>
+  <body>
+    <p>
+<?php
+  echo $_GET['lastname'] . ' ' . $_GET['firstname'] . ' ' . $_GET['age'];
+?>
+    </p>
+  </body>
+</html>
